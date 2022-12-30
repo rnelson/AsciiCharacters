98b8f4950bf9e9c5de61cd00d26e771dac494859
Ross Nelson <ross.nelson@gmail.com>
2022-12-30 14:52:45 -0600

    Attempts to make it work

--- mk.sh
+++ mk.sh
@@ -3,10 +3,10 @@
 
 as src/boot.s -o obj/boot.o
 
-gcc -Iinclude -c src/vga.c -o obj/vga.o -std=c99 -ffreestanding
-gcc -Iinclude -c src/main.c -o obj/main.o -std=c99 -ffreestanding
+gcc -Iinclude -c src/vga.c -o obj/vga.o -std=c99 -ffreestanding -nostdlib -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2
+gcc -Iinclude -c src/main.c -o obj/main.o -std=c99 -ffreestanding -nostdlib -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2
 
-gcc -T linker.ld -o iso/boot/AsciiCharacters.bin -ffreestanding -nostdlib obj/boot.o obj/vga.o obj/main.o -lgcc
+gcc -T linker.ld -o iso/boot/AsciiCharacters.bin -ffreestanding -nostdlib -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 obj/boot.o obj/vga.o obj/main.o -lgcc
 
 grub2-mkrescue -o AsciiCharacters.iso iso
 #virt-install --name=AsciiCharacters --ram=16 --vcpus=1 --os-variant generic --cdrom=AsciiCharacters.iso --boot cdrom --disk none
